405c1783495041255d0110e0c1ed779e5a118a52
shenqing <shenqin1@msu.edu>
2023-06-08 12:41:45 -0400

    Initial Commit

---
+++ AirVisual/AirVisualApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirVisualApp.swift
+//  AirVisual
+//
+//  Created by 沈清昊 on 6/8/23.
+//
+
+import SwiftUI
+
+@main
+struct AirVisualApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
